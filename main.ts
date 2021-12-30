ab0cfd2453ace416e31a81f22cf7d87f2219f567
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-12-30 11:00:45 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,6 +20,7 @@ function doMissle () {
     missle.delete()
 }
 input.onButtonPressed(Button.A, function () {
+    music.playTone(262, music.beat(BeatFraction.Eighth))
     if (defender.get(LedSpriteProperty.X) > 0) {
         defender.change(LedSpriteProperty.X, -1)
     }
@@ -28,8 +29,16 @@ input.onButtonPressed(Button.AB, function () {
     doMissle()
 })
 input.onButtonPressed(Button.B, function () {
+    music.playTone(523, music.beat(BeatFraction.Eighth))
     if (defender.get(LedSpriteProperty.X) < 4) {
         defender.change(LedSpriteProperty.X, 1)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    music.playTone(131, music.beat(BeatFraction.Whole))
+    droid += 1
+    if (droid > 1) {
+        droid = 0
     }
 })
 let rock1: game.LedSprite = null
@@ -37,7 +46,9 @@ let rock2: game.LedSprite = null
 let missle: game.LedSprite = null
 let defender: game.LedSprite = null
 let droid = 0
+droid = 0
 defender = game.createSprite(2, 4)
+game.setLife(10)
 basic.forever(function () {
     basic.pause(2000)
     while (true) {
@@ -46,6 +57,9 @@ basic.forever(function () {
         while (!(rock1.isTouchingEdge())) {
             rock1.move(1)
             basic.pause(300)
+            if (rock1.isTouching(defender)) {
+                game.removeLife(1)
+            }
         }
         rock1.delete()
     }
@@ -58,7 +72,20 @@ basic.forever(function () {
         while (!(rock2.isTouchingEdge())) {
             rock2.move(1)
             basic.pause(200)
+            if (rock2.isTouching(defender)) {
+                game.removeLife(1)
+            }
         }
         rock2.delete()
+    }
+})
+basic.forever(function () {
+    if (droid == 1) {
+        defender.set(LedSpriteProperty.X, rock1.get(LedSpriteProperty.X))
+        doMissle()
+        basic.pause(100)
+        defender.set(LedSpriteProperty.X, rock2.get(LedSpriteProperty.X))
+        doMissle()
+        basic.pause(100)
     }
 })
